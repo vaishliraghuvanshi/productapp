4ec7c0ef34a54197f68aa9ca0e44bd7fb472bf98
vaishaliraghuvanshi <vaishali24raghuvanshi@gmail.com>
2022-04-02 14:31:45 +0530

    it is

---
+++ middleware/token_verification.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+exports.verifyToken = (request, response, next) => {
+    try {
+        console.log('token ' + request.headers.authorization);
+        if (!request.headers.authorization)
+          return response.status(401).send('unauthorized request');
+        if(request.headers.authorization==null)
+          return response.status(401).send('unauthorized request');
+          
+      let token=request.headers.authorization.split(" ")[1];
+      let payload=jwt.verify(token,'aaabbbccc');
+      console.log(payload);
+      next();    
+    }
+    catch(err){
+        return response.status(401).send('unauthorized request');
+    }
+}
