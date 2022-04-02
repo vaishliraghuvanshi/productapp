61a785fb78bff6312776f7d4bc500eaefbbe50f4
vaishaliraghuvanshi <vaishali24raghuvanshi@gmail.com>
2022-04-02 15:42:12 +0530

    it is

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 mongoose.connect("mongodb+srv://vaishali:abcd@cluster0.iqght.mongodb.net/productapi?retryWrites=true&w=majority")
-const cors = require('cors');
 const bodyparser = require('body-parser');
 
 
 const app = express();
 const port = process.env.PORT|| 3000
+const cors = require('cors');
 
 
 app.use(cors());
